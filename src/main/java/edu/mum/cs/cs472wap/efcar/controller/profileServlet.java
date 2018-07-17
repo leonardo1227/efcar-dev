3cf33199795ee5825648fe3aa4844544401007a9
Edwin Cobos <alejo740@gmail.com>
2018-07-16 22:11:09 -0500

    Finished Backend to send updated profile data to data the DB

--- src/main/java/edu/mum/cs/cs472wap/efcar/controller/profileServlet.java
+++ src/main/java/edu/mum/cs/cs472wap/efcar/controller/profileServlet.java
@@ -1,17 +1,63 @@
 package edu.mum.cs.cs472wap.efcar.controller;
 
+import edu.mum.cs.cs472wap.efcar.data.DataService;
+import edu.mum.cs.cs472wap.efcar.model.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+
+import static edu.mum.cs.cs472wap.efcar.Util.Property.SESSION_USER_ATTRIBUTE_NAME;
 
 @WebServlet("/profile")
 public class profileServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String change = request.getParameter("change");
+        request.setAttribute("change", change);
         request.getRequestDispatcher("/WEB-INF/pages/profile.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String changeType = request.getParameter("changeType");
+
+        if(changeType.equals("info")) {
+            String userName = request.getParameter("userName");
+            String firstName = request.getParameter("firstName");
+            String lastName = request.getParameter("lastName");
+            String dateOfBirth = request.getParameter("dateOfBirth");
+            String gender = request.getParameter("gender");
+            String phone = request.getParameter("phone");
+            String email = request.getParameter("email");
+            String line1 = request.getParameter("line1");
+            String line2 = request.getParameter("line2");
+            String city = request.getParameter("city");
+            String state = request.getParameter("state");
+            String zipCode = request.getParameter("zipCode");
+
+            User user = (User)request.getSession().getAttribute(SESSION_USER_ATTRIBUTE_NAME);
+            user.setUsername(userName);
+            user.setFirstName(firstName);
+            user.setLastName(lastName);
+            user.setDateOfBirth(LocalDate.parse(dateOfBirth));
+            user.setGender(gender);
+            user.getContact().setPhoneNumber(phone);
+            user.getContact().setEmail(email);
+            user.getAddress().setLine1(line1);
+            user.getAddress().setLine2(line2);
+            user.getAddress().setCity(city);
+            user.getAddress().setState(state);
+            user.getAddress().setZipCode(zipCode);
+
+            response.sendRedirect(request.getContextPath() + "/profile?change=1");
+        }else if(changeType.equals("password")) {
+            response.sendRedirect(request.getContextPath() + "/profile?change=2");
+        }
     }
 }
--- web/resources/js/registration.js
+++ web/resources/js/registration.js
@@ -1,6 +1,6 @@
 $(function () {
-    var password = $("#passwordId");
-    var passwordConfirm = $("#passwordConfirmId");
+    var password = $("#newPassword");
+    var passwordConfirm = $("#newPasswordConfirm");
     function validatePassword() {
         if (password.val() !== passwordConfirm.val()) {
             passwordConfirm[0].setCustomValidity("Passwords Don't Match");
@@ -8,20 +8,98 @@ $(function () {
             passwordConfirm[0].setCustomValidity('');
         }
     }
-    password.change(validatePassword);
-    passwordConfirm.keyup(validatePassword);
+
+    $('#passwordForm').validate({
+        errorElement: 'div',
+        rules: {
+            oldPassword: {
+                required: true,
+                minLength: 8
+            },
+            newPassword: {
+                required: true,
+                minLength: 8
+            },
+            newPasswordConfirm: {
+                required: true,
+                equalTo: '#newPassword'
+            }
+        },
+        messages: {
+            oldPassword: "Write your old password",
+            newPassword: "Inser a valid password",
+            newPasswordConfirm: "Write the same password"
+        },
+        submitHandler: function(form) {
+            form.submit();
+        }
+    });
+
+
 
     $('#informationForm').validate({ // initialize the plugin
         errorElement: 'div',
         rules: {
+            userName: {
+                required: true,
+                minlength: 3
+            },
             firstName: {
                 required: true,
-                minlength: 5
+                minlength: 2
             },
             lastName: {
                 required: true,
-                minlength: 5
+                minlength: 2
+            },
+            dateOfBirth: {
+                required: true,
+                minlength: 2
+            },
+            gender: {
+                required: true,
+            },
+            phone: {
+                required: true,
+                minlength: 2
+            },
+            email: {
+                required: true,
+                minlength: 2
+            },
+            line1: {
+                required: true,
+                minlength: 2
+            },
+            city: {
+                required: true,
+                minlength: 2
+            },
+            state: {
+                required: true,
+                minlength: 2
+            },
+            zipCode: {
+                required: true,
+                minlength: 2
             }
+        },
+        messages: {
+            userName: "Please insert a valid user name",
+            firstName: "Please insert a valid first name",
+            lastName: "Please insert a valid last name",
+            dateOfBirth: "Please insert a valid date Of birth",
+            gender: "Please insert a valid gender",
+            phone: "Please insert a valid phone",
+            email: "Please insert a valid email",
+            line1: "Please insert a valid line 1",
+            line2: "Please insert a valid line 2",
+            city: "Please insert a valid city",
+            state: "Please insert a valid state",
+            zipCode: "Please insert a valid zip Code",
+        },
+        submitHandler: function(form) {
+            form.submit();
         }
     });
 });
